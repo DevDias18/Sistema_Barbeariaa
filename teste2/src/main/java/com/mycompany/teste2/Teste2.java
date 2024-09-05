7935e47400245d05e0c21a22900f7b05fbae682a
DevDias18 <geovannifvl18@gmail.com>
2024-09-04 22:29:00 -0300

    dsd

---
+++ teste2/src/main/java/com/mycompany/teste2/Teste2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.teste2;
+
+/**
+ *
+ * @author geova
+ */
+public class Teste2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
